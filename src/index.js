44464d08791a60cf1697c65a7856fa832e0d66f2
Anton81951 <antonbugaichuk@gmail.com>
2024-07-10 14:19:40 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { initMongoConnection } from '../src/db/initMongoConnection.js';
+import { setupServer } from './server.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
