3d823e2f4d90bb66ad41cbd0d633ed52144d157d
Anton81951 <antonbugaichuk@gmail.com>
2024-07-11 18:30:34 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -32,17 +32,9 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     try {
       const contacts = await getAllContacts();
-      res.status(200).json({
-        status: res.status,
-        data: contacts,
-        message: 'Successfully found contacts!',
-      });
+      res.status(200).json({ status: res.statusCode, data: contacts, message: 'Successfully found contacts!' });
     } catch (error) {
-      res.status(500).json({
-        status: res.status,
-        message: 'Failed to fetch contacts',
-        error: error.message,
-      });
+      res.status(500).json({ status: res.statusCode, message: 'Failed to fetch contacts', error: error.message });
     }
   });
 
@@ -51,23 +43,12 @@ export const setupServer = () => {
     try {
       const contact = await getContactById(contactId);
       if (!contact) {
-        res.status(404).json({
-          status: res.status,
-          message: 'Contact not found',
-        });
+        res.status(404).json({ message: 'Contact not found' });
         return;
       }
-      res.status(200).json({
-        status: res.status,
-        data: contact,
-        message: `Successfully found contact with id ${contactId}!`,
-      });
+      res.status(200).json({ status: res.statusCode, data: contact, message: `Successfully found contact with id ${contactId}!` });
     } catch (error) {
-      res.status(500).json({
-        status: res.status,
-        message: 'Failed to fetch contact',
-        error: error.message,
-      });
+      res.status(500).json({ status: res.statusCode, message: 'Failed to fetch contact', error: error.message });
     }
   });
 
