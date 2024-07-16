2f111e41830856af95dee2b7da00d0337f401b6b
Anton81951 <antonbugaichuk@gmail.com>
2024-07-16 13:40:28 +0300

    1

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-import createHttpError from 'http-errors';
+const createHttpError = require('http-errors');
 
 export const errorHandler = (err, req, res, next) => {
     if (createHttpError.isHttpError(err)) {
